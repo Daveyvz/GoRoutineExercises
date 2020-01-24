d1e612f2f4a41fc8a0b1da0431b77fcc435e704f
Davey <davey_vanz@live.nl>
2020-01-24 14:14:57 +0100

    finished hands_on_4 Mutex

--- Hands_on_3/main.go
+++ Hands_on_3/main.go
@@ -8,30 +8,39 @@ import (
 var wg sync.WaitGroup
 
 func main() {
+	var wg sync.WaitGroup
 	count := 0
 	wg.Add(3)
+	var mu sync.Mutex
+
 	go func() {
+		mu.Lock()
 		v := count
 		runtime.Gosched()
 		v++
 		count = v
 		wg.Done()
+		mu.Unlock()
 	}()
 
 	go func() {
+		mu.Lock()
 		v := count
 		runtime.Gosched()
 		v++
 		count = v
 		wg.Done()
+		mu.Unlock()
 	}()
 
 	go func() {
+		mu.Lock()
 		v := count
 		runtime.Gosched()
 		v++
 		count = v
 		wg.Done()
+		mu.Unlock()
 	}()
 
 	wg.Wait()
